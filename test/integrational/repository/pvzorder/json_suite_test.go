4dbd593a24cbc8d72654ab5af06c048c68fabb31
semyon danshin <semen.danshin@gmail.com>
2024-09-20 01:44:40 +0300

    feat(hw3): Add integrational tests for repository with testify test suites.

---
+++ test/integrational/repository/pvzorder/json_suite_test.go
@@ -0,0 +1,208 @@
+package pvzorder
+
+import (
+	"errors"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"homework/internal/abstractions"
+	"homework/internal/domain"
+	"homework/internal/infrastructure/repositories/pvzorder"
+	"os"
+	"testing"
+	"time"
+)
+
+type JSONRepositorySuite struct {
+	suite.Suite
+	storage *pvzorder.JSONRepository
+}
+
+func (s *JSONRepositorySuite) SetupTest() {
+	fileName := fmt.Sprintf("test_%v.json", time.Now().Unix())
+
+	err := copyFile("sample_data.json", fileName)
+	s.Require().NoError(err)
+
+	s.storage = pvzorder.NewJSONRepository(fileName)
+
+	s.T().Cleanup(func() {
+		err := os.Remove(fileName)
+		s.Require().NoError(err)
+	})
+}
+
+func (s *JSONRepositorySuite) TestCreateOrder() {
+	order := domain.NewPVZOrder(
+		"100",
+		"1",
+		"1",
+		1000,
+		1000,
+		24*time.Hour,
+		domain.PackagingTypeBox,
+		false,
+	)
+
+	err := s.storage.CreateOrder(order)
+	s.Require().NoError(err)
+
+	actualOrder, err := s.storage.GetOrder("100")
+	s.Require().NoError(err)
+
+	s.Equal(order.ReceivedAt.UnixMilli(), actualOrder.ReceivedAt.UnixMilli())
+	order.ReceivedAt = actualOrder.ReceivedAt
+	s.Equal(order, actualOrder)
+}
+
+func (s *JSONRepositorySuite) TestDeleteOrder() {
+	err := s.storage.DeleteOrder("1")
+	s.Require().NoError(err)
+
+	_, err = s.storage.GetOrder("1")
+	s.Error(err)
+	s.ErrorIs(err, domain.ErrNotFound)
+}
+
+func (s *JSONRepositorySuite) TestSetOrderIssued() {
+	err := s.storage.SetOrderIssued("1")
+	s.Require().NoError(err)
+
+	order, err := s.storage.GetOrder("1")
+	s.Require().NoError(err)
+
+	s.NotZero(order.IssuedAt)
+}
+
+func (s *JSONRepositorySuite) TestSetOrderReturned() {
+	err := s.storage.SetOrderReturned("1")
+	s.Require().NoError(err)
+
+	order, err := s.storage.GetOrder("1")
+	s.Require().NoError(err)
+
+	s.NotZero(order.ReturnedAt)
+}
+
+func (s *JSONRepositorySuite) TestGetOrders() {
+	type args struct {
+		userID string
+		opts   []abstractions.GetOrdersOptFunc
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want func([]domain.PVZOrder) bool
+	}{
+		{
+			name: "Success",
+			args: args{
+				userID: "1",
+				opts:   make([]abstractions.GetOrdersOptFunc, 0),
+			},
+			want: func(orders []domain.PVZOrder) bool {
+				return s.Len(orders, 3)
+			},
+		},
+		{
+			name: "Success with PVZID",
+			args: args{
+				userID: "1",
+				opts: []abstractions.GetOrdersOptFunc{
+					abstractions.WithPVZID("2"),
+				},
+			},
+			want: func(orders []domain.PVZOrder) bool {
+				return s.Len(orders, 1) && s.Equal("3", orders[0].OrderID)
+			},
+		},
+		{
+			name: "Success with pagination",
+			args: args{
+				userID: "1",
+				opts: []abstractions.GetOrdersOptFunc{
+					abstractions.WithCursorID("3"),
+					abstractions.WithLimit(2),
+				},
+			},
+			want: func(orders []domain.PVZOrder) bool {
+				return s.Len(orders, 2) && s.Equal("3", orders[0].OrderID) && s.Equal("2", orders[1].OrderID)
+			},
+		},
+		{
+			name: "Success empty",
+			args: args{
+				userID: "3",
+				opts:   make([]abstractions.GetOrdersOptFunc, 0),
+			},
+			want: func(orders []domain.PVZOrder) bool {
+				return s.Len(orders, 0)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		s.Run(tt.name, func() {
+			orders, err := s.storage.GetOrders(tt.args.userID, tt.args.opts...)
+			tt.want(orders)
+			s.NoError(err)
+		})
+	}
+}
+
+func (s *JSONRepositorySuite) TestGetOrder() {
+	type args struct {
+		orderID string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    domain.PVZOrder
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "Success",
+			args: args{
+				orderID: "1",
+			},
+			want: domain.PVZOrder{
+				OrderID:        "1",
+				PVZID:          "1",
+				RecipientID:    "1",
+				ReceivedAt:     time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+				StorageTime:    24 * time.Hour,
+				IssuedAt:       time.Time{},
+				ReturnedAt:     time.Time{},
+				Weight:         1000,
+				Cost:           1000,
+				AdditionalFilm: false,
+				Packaging:      domain.PackagingTypeBox,
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "Order not found",
+			args: args{
+				orderID: "100",
+			},
+			want: domain.PVZOrder{},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				return assert.Error(t, err) && errors.Is(err, domain.ErrNotFound)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		s.Run(tt.name, func() {
+			order, err := s.storage.GetOrder(tt.args.orderID)
+			tt.wantErr(s.T(), err)
+			s.Equal(tt.want, order)
+		})
+	}
+}
+
+func TestJSONRepository_Run(t *testing.T) {
+	suite.Run(t, &JSONRepositorySuite{})
+}
