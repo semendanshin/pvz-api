77ba352f3be409d486687c01e647f0e594b49100
semyon danshin <semen.danshin@gmail.com>
2024-09-24 16:20:40 +0300

    feat(hw4): Add indexes to db.

---
+++ migrations/00002_add_indexes.sql
@@ -0,0 +1,16 @@
+-- +goose NO TRANSACTION
+-- +goose Up
+CREATE INDEX CONCURRENTLY idx_pvz_order_recipient_id ON pvz_orders (recipient_id);
+CREATE INDEX CONCURRENTLY idx_pvz_order_pvz_id ON pvz_orders (pvz_id);
+CREATE INDEX CONCURRENTLY idx_pvz_order_returned_at ON pvz_orders (returned_at);
+CREATE INDEX CONCURRENTLY idx_pvz_order_received_at ON pvz_orders (received_at);
+CREATE INDEX CONCURRENTLY idx_pvz_order_deleted_at_not_deleted ON pvz_orders (deleted_at) WHERE deleted_at IS NULL;
+
+-- +goose Down
+-- +goose StatementBegin
+DROP INDEX CONCURRENTLY idx_pvz_order_recipient_id;
+DROP INDEX CONCURRENTLY idx_pvz_order_pvz_id;
+DROP INDEX CONCURRENTLY idx_pvz_order_returned_at;
+DROP INDEX CONCURRENTLY idx_pvz_order_received_at;
+DROP INDEX CONCURRENTLY idx_pvz_order_deleted_at_not_deleted;
+-- +goose StatementEnd
