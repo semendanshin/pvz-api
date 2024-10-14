671bee826e72659ffa23f3a8016aafd1393eeaaa
semyon danshin <semen.danshin@gmail.com>
2024-10-14 18:11:05 +0300

    feat: Add events sender use case. Add entrypoint for event sender.

---
+++ cmd/kafka-sender/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"homework/internal/usecases"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func loadPostgresURL() string {
+	postgresHost := os.Getenv("POSTGRES_HOST")
+	postgresPort := os.Getenv("POSTGRES_PORT")
+	postgresUsername := os.Getenv("POSTGRES_USERNAME")
+	postgresPassword := os.Getenv("POSTGRES_PASSWORD")
+	postgresDatabase := os.Getenv("POSTGRES_DATABASE")
+
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", postgresHost, postgresPort, postgresUsername, postgresPassword, postgresDatabase)
+}
+
+func Run() error {
+	err := godotenv.Load()
+	if err != nil {
+		return fmt.Errorf("error loading .env file %w", err)
+	}
+
+	postgresURL := loadPostgresURL()
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	pool, err := pgxpool.New(ctx, postgresURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	worker := usecases.NewEventsSender(nil, nil)
+
+	go func() {
+		stop := make(chan os.Signal, 1)
+		signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+		<-stop
+
+		worker.Stop()
+	}()
+
+	if err := worker.Run(ctx, 5*time.Second); err != nil {
+		return fmt.Errorf("error running worker: %w", err)
+	}
+
+	return nil
+}
+
+func main() {
+	if err := Run(); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
---
+++ internal/usecases/events.go
@@ -0,0 +1,92 @@
+package usecases
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+
+	"homework/internal/domain"
+
+	_ "github.com/gojuno/minimock/v3"
+)
+
+//go:generate go run github.com/gojuno/minimock/v3/cmd/minimock -g -i EventsRepository -s _mock.go -o ./mocks
+//go:generate go run github.com/gojuno/minimock/v3/cmd/minimock -g -i KafkaClient -s _mock.go -o ./mocks
+
+type EventsRepository interface {
+	// GetPendingEvents returns a list of events that have not been sent yet.
+	GetPendingEvents(ctx context.Context) ([]domain.Event, error)
+	// MarkAsSent marks the event as sent.
+	MarkAsSent(ctx context.Context, id uuid.UUID) error
+}
+
+type KafkaClient interface {
+	// SendEvent sends an event to Kafka.
+	SendEvent(ctx context.Context, event domain.Event) error
+}
+
+type EventsSender struct {
+	repo   EventsRepository
+	client KafkaClient
+
+	done chan struct{}
+}
+
+func NewEventsSender(repo EventsRepository, client KafkaClient) *EventsSender {
+	return &EventsSender{
+		repo:   repo,
+		client: client,
+		done:   make(chan struct{}),
+	}
+}
+
+func (e *EventsSender) Run(ctx context.Context, interval time.Duration) error {
+	ticker := time.NewTicker(interval)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-e.done:
+			return nil
+		case <-ticker.C:
+			if err := e.RunOnce(ctx); err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func (e *EventsSender) Stop() {
+	close(e.done)
+}
+
+func (e *EventsSender) RunOnce(ctx context.Context) error {
+	events, err := e.repo.GetPendingEvents(ctx)
+	if err != nil {
+		return fmt.Errorf("error getting pending events: %w", err)
+	}
+
+	for _, event := range events {
+		fmt.Println(event)
+		if err := e.processEvent(ctx, event); err != nil {
+			return fmt.Errorf("error processing event: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (e *EventsSender) processEvent(ctx context.Context, event domain.Event) error {
+	if err := e.client.SendEvent(ctx, event); err != nil {
+		return fmt.Errorf("error sending event: %w", err)
+	}
+
+	if err := e.repo.MarkAsSent(ctx, event.ID); err != nil {
+		return fmt.Errorf("error marking event as sent: %w", err)
+	}
+
+	return nil
+}
