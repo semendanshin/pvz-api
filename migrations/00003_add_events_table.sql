7b9c70bb19ae7080ce34e00662bf9045b8393f6b
semyon danshin <semen.danshin@gmail.com>
2024-10-14 17:16:07 +0300

    feat: Add event table to db.

---
+++ migrations/00003_add_events_table.sql
@@ -0,0 +1,14 @@
+-- +goose NO TRANSACTION
+-- +goose Up
+CREATE TABLE IF NOT EXISTS events (
+    id uuid PRIMARY KEY,
+    event_type VARCHAR(255) NOT NULL,
+    payload JSONB NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_events_created_at ON events (created_at);
+
+-- +goose NO TRANSACTION
+-- +goose Down
+DROP TABLE IF EXISTS events;
+DROP INDEX CONCURRENTLY IF EXISTS idx_events_created_at;
